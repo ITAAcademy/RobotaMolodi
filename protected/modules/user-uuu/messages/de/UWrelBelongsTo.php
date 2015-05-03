b51000c6e9b721b17ce32b6ce2ec9e9f117c5b16
Oleksii Tiurenkov <alterego4@gmail.com>
2015-05-03 12:24:54 +0300

    no message

---
+++ protected/modules/user-uuu/messages/de/UWrelBelongsTo.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'Model Name'=>'Model-Name',
+	'Lable field name'=>'Bezeichner Feldnamen',
+	'Empty item name'=>'Leerer Artikelname',
+	'Profile model relation name'=>'Profil-Model Relationsname',
+);
