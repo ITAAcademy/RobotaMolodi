751f841f11f328d1943849ac2a91748521aba96e
Oleksii Tiurenkov <alterego4@gmail.com>
2015-05-17 19:38:04 +0300

    Fix for menu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -43,6 +43,7 @@
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
+                                                                <li><a href="{{ url('/home') }}">Особистий кабінет</a></li>
 								<li><a href="{{ url('/auth/logout') }}">Вийти</a></li>
 							</ul>
 						</li>
