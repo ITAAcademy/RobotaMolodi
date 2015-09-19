bd49473546aded12d019ec72c60f04f9c64f5841
Inna Kocherghuk <innakocherzhuk@gmail.com>
2015-09-19 16:03:20 +0300

    ch views

--- resources/views/Resume/showMyResume.blade.php
+++ resources/views/Resume/showMyResume.blade.php
@@ -19,21 +19,6 @@
         </ul>
     </div>
 
-
-  
-    <div class="panel panel-orange">
-        <div class="panel-heading"><h3>{!!$resume->position!!} </h3></div>
-        <ul class="list-group">
-            <li class="list-group-item">  Ім'я :  {!!$resume->name_u!!}</li>
-            <li class="list-group-item">  Позиція :  {!!$resume->position!!}</li>
-            <li class="list-group-item">   Місто  :  {!!$resume->city!!}</li>
-            <li class="list-group-item">  Промисловість: {!!$resume->industry!!}</li>
-            <li class="list-group-item">  Зарплата: {!!$resume->salary!!} грн.</li>
-            <li class="list-group-item"> Опис: {!!$resume->description!!} <span class="text-muted text-right pull-right"><h5>{{ date('j.m.Y,H:i:s', strtotime($resume->created_at))}}</h5></span></li>
-        </ul>
-    </div>
-
-
     <script>
 
         function ConfirmDelete()
