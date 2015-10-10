a33a335f8dbcd783181a705eabe9af13befeb43e
Sorey <3sorey4@gmail.com>
2015-10-10 16:12:55 +0300

    fexed RM-75( link company)

--- resources/views/vacancy/showMyVacancy.blade.php
+++ resources/views/vacancy/showMyVacancy.blade.php
@@ -9,8 +9,7 @@
     <div class="panel panel-orange">
         <div class="panel-heading"><h2>{{$vacancy->position}} &#183; {{$vacancy->salary}} грн</h2></div>
         <ul class="list-group">
-
-            <li class="list-group-item"> <a href="#">{{$company->company_name}}</a> &#183; {{$user->name}}</li>
+            <li class="list-group-item">  <a href="@if($company->company_email != ''){{$company->company_email}} @else #@endif">{{$company->company_name}}</a>,{{$user->name}}  </li>
             <li class="list-group-item"> @foreach($cities as $city) {{$city->name}} @endforeach</li>
             <li class="list-group-item">  {{$industry->name}}</li>
             <li class="list-group-item"><span class="heading"> Опис : </span> {{$vacancy->description}} <span class="text-muted text-right pull-right"><h5>{{ date('j.m.Y,H:i:s', strtotime($vacancy->created_at))}}</h5></span></li>
