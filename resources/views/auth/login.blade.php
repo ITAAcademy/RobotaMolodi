0a2bbf9dbc70f5f2f6d711c2c64ae747ab50872a
Inna Kocherghuk <innakocherzhuk@gmail.com>
2015-10-12 19:18:38 +0300

    RM-142 (translate error's text into uk)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
+							<strong>Ой!</strong> Виникла якась проблема.<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
 									<li>{{ $error }}</li>
