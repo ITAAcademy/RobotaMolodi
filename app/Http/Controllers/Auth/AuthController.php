c0b01006132d648b61224e358019559dc4baeb56
awalio <awalings@gmail.com>
2015-12-12 11:05:10 +0200

    Upgraded to 5.1

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,5 +1,9 @@
-<?php namespace App\Http\Controllers\Auth;
+<?php
+namespace App\Http\Controllers\Auth;
+// namespace;
 
+use App\Models\User;
+use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\Registrar;
@@ -28,11 +32,12 @@ class AuthController extends Controller {
 	 * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
 	 * @return void
 	 */
-	public function __construct(Guard $auth, Registrar $registrar)
+	// public function __construct(Guard $auth, Registrar $registrar)
+	public function __construct()
 	{
 
-		$this->auth = $auth;
-		$this->registrar = $registrar;
+		// $this->auth = $auth;
+		// $this->registrar = $registrar;
 
 		$this->middleware('guest', ['except' => 'getLogout']);
 	}
@@ -44,5 +49,36 @@ class AuthController extends Controller {
 //	{
 //		return redirect()->back();
 //	}
+		/**
+		 * Get a validator for an incoming registration request.
+		 *
+		 * @param  array  $data
+		 * @return \Illuminate\Contracts\Validation\Validator
+		 */
+		public function validator(array $data)
+		{
+			return Validator::make($data, [
+		//			'name' => 'required|max:255|alpha',
+		//			'email' => 'required|email|max:255|unique:users',
+		//			'password' => 'required|confirmed|min:6',
+				'name' => 'required|max:30|regex:/[^a-z,A-Z,0-9,�-������,�-߲���,-,_,]/',
+				'email' => 'required|email|max:30|unique:users',
+				'password' => 'required|confirmed|min:6',
+			]);
+		}
 
+		/**
+		 * Create a new user instance after a valid registration.
+		 *
+		 * @param  array  $data
+		 * @return User
+		 */
+		public function create(array $data)
+		{
+			return User::create([
+				'name' => $data['name'],
+				'email' => $data['email'],
+				'password' => bcrypt($data['password']),
+			]);
+		}
 }
