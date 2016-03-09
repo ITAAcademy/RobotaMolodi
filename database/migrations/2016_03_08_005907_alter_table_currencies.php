79ad32413489f065f8370314ee2d14353ad2f722
Nick Zorych <neo.vsmu@gmail.com>
2016-03-09 08:55:52 +0200

    add currency scheduler

---
+++ database/migrations/2016_03_08_005907_alter_table_currencies.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableCurrencies extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('currencies', function ($table) {
+            $table->timestamp('created_at');
+        });
+        Schema::table('currencies', function ($table) {
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('currencies', function ($table) {
+            $table->dropColumn('created_at');
+        });
+        Schema::table('currencies', function ($table) {
+            $table->dropColumn('updated_at');
+        });
+    }
+}
