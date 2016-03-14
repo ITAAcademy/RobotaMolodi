fff15994689f26a5775ab26cfeae5d0e957da15d
Morgunov <morgunov.igor1990@gmail.com>
2016-03-14 21:21:09 +0200

    Fixed aftermerge conflicts

--- resources/views/main/filter/filterVacancies.blade.php
+++ resources/views/main/filter/filterVacancies.blade.php
@@ -3,13 +3,134 @@
     <h4 class="btn btn-default" style="background:wheat; color:#ffffff;">{!! link_to_route('resume.create', 'Написати резюме') !!}</h4>
 @stop
 @section('panelTitle')
-    <li role = "presentation" class="active"><a href={{route('head')}}><span>{!! Html::image('image/allvacancies.png','Vac',['id'=>'allvacancies']) !!}</span> Всі вакансії</a></li>
-    <li role = "presentation"><a href={{ route('main.resumes')}}><span>{!! Html::image('image/allresumes.png','Res',['id'=>'allresumes']) !!}</span> Всі резюме</a></li>
-	<li role = "presentation"><a href={{route('main.companies')}}><span>{!! Html::image('image/allcompanies.png','Comp',['id'=>'allcompanies']) !!}</span> Всі компанії</a></li>
+    <li role = "presentation" class="active"><a href={{route('head','Всі вакансії')}}><span>{!! Html::image('image/allvacancies.png','Vac',['id'=>'allvacancies']) !!}</span> Всі вакансії</a></li>
+    <li role = "presentation"><a href={{route('main.resumes' ,'Всі резюме')}}><span>{!! Html::image('image/allresumes.png','Res',['id'=>'allresumes']) !!}</span> Всі резюме</a></li>
+	   <li role = "presentation"><a href={{route('main.companies')}}><span>{!! Html::image('image/allcompanies.png','Comp',['id'=>'allcompanies']) !!}</span> Всі компанії</a></li>
 @stop
 
 @section('category')
+
     <div class="posts">
-        @include('vacancy._vacancy')
+        @include('main.filter.vacancy')
     </div>
+
+
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script src="js/pagination.js"></script>
+    <script>
+
+        $(window).on('hashchange', function() {
+            if (window.location.hash) {
+                var page = window.location.hash.replace('#', '');
+                if (page == Number.NaN || page <= 0) {
+                    return false;
+                } else {
+                    getPosts(page);
+                }
+            }
+        });
+        $(document).ready(function() {
+            $(document).on('click','.pagination a' , function (e) {
+                getPosts($(this).attr('href').split('page=')[1]);
+                e.preventDefault();
+            });
+            /////////////////////////////////////
+            $('#selectIndustry').change(function(){
+                $("div.list-group").empty();
+                var city_id = $('[name=city]').val();
+                var industry_id = $('[name=industry]').val();
+				        var specialisation = $('[name=spec]').val();
+                var search_bool = '{{$search_boolean}}';
+
+                if(search_bool=="true"){
+                var url = 'searchVacancies';
+                }
+                else{
+                  var url = 'showVacancies';
+                }
+          $.ajax({
+							url: url,
+							type: "POST",
+							 beforeSend: function (xhr) {
+									var token = $('meta[name="csrf_token"]').attr('content');
+
+									if (token) {
+										return xhr.setRequestHeader('X-CSRF-TOKEN', token);
+									}
+								},
+							data: {'specialisation_': specialisation,'city_id': city_id, 'industry_id': industry_id,data:'{{$data}}'},
+							success: function (json) {
+								$('.posts').html(json);
+
+							}
+						});
+            });
+            /////////////////////////////////
+            $('#selectCity').change(function(){
+                $("div.list-group").empty();
+                var city_id = $('[name=city]').val();
+                var industry_id = $('[name=industry]').val();
+				        var specialisation = $('[name=spec]').val();
+                var search_bool = '{{$search_boolean}}';
+
+                if(search_bool=="true"){
+                var url = 'searchVacancies';
+                }
+                else{
+                  var url = 'showVacancies';
+                }
+			$.ajax({
+							url: url,
+							type: "POST",
+							 beforeSend: function (xhr) {
+									var token = $('meta[name="csrf_token"]').attr('content');
+
+									if (token) {
+										return xhr.setRequestHeader('X-CSRF-TOKEN', token);
+									}
+								},
+							data: {'specialisation_': specialisation,'city_id': city_id, 'industry_id': industry_id,data:'{{$data}}'},
+							success: function (json) {
+								$('.posts').html(json);
+
+							}
+						});
+
+
+            });
+			  $('#selectSpecialisation').change(function(){
+                $("div.list-group").empty();
+                var city_id = $('[name=city]').val();
+                var industry_id = $('[name=industry]').val();
+				        var specialisation = $('[name=spec]').val();
+                var search_bool = '{{$search_boolean}}';
+
+                if(search_bool=="true"){
+                var url = 'searchVacancies';
+                }
+                else{
+                  var url = 'showVacancies';
+                }
+
+				$.ajax({
+							url: url,
+							type: "POST",
+							 beforeSend: function (xhr) {
+									var token = $('meta[name="csrf_token"]').attr('content');
+
+									if (token) {
+										return xhr.setRequestHeader('X-CSRF-TOKEN', token);
+									}
+								},
+							data: {'specialisation_': specialisation,'city_id': city_id, 'industry_id': industry_id,data:'{{$data}}'},
+							success: function (json) {
+								$('.posts').html(json);
+
+							}
+						});
+            });
+        });
+
+    </script>
 @stop
---
+++ resources/views/main/filter/vacancy.blade.php
@@ -0,0 +1,29 @@
+@if(count($vacancies) === 0)
+    <br>
+    <?php echo "Немає вакансій по Вашому пошуку"?>
+
+@else
+@foreach ($vacancies as $vacancy)
+
+    <article>
+        <a href="/vacancy/{{$vacancy->id}}" class="link">
+            <div class="list">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h2 class="list-group-item-heading panel-title"><span class="text-info" >{{$vacancy->position}} </span> &#183;  {{$vacancy->salary}} - {{$vacancy->salary_max}} {{$vacancy->Currency()[0]['currency']}}
+                            <span class="text-muted text-right pull-right"><h5>{{ date('j.m.Y, H:i:s', strtotime($vacancy->created_at))}}</h5></span></h2></div>
+                    <div class="panel-body">
+                        <h4 class="list-group-item-heading">{{ $vacancy->Industry()->name}}</h4>
+                        <h4 class="list-group-item-heading">{{ $vacancy->Company()->company_name}}</h4>
+                        <h4 class="list-group-item-heading">@foreach($vacancy->City() as $city){{ $city->name}} @endforeach</h4>
+                    </div>
+                </div>
+            </div>
+
+            </a>
+    </article>
+
+@endforeach
+@include('/pagination/pagination', ['paginator' => $vacancies])
+{{--{!!$vacancies->appends(['city_id' => $city_id, 'industry_id' => $industry_id])->render()!!}--}}
+@endif
