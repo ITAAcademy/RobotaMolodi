494bc0b2496f355eab4016422b732570a956b679
root <root@intita.com>
2016-04-09 13:46:24 +0200

    Fix rights for public

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'rm'),
             'username'  => env('DB_USERNAME', 'rm'),
-            'password'  => env('DB_PASSWORD', '1'),
+            'password'  => env('DB_PASSWORD', 'GfyR2naS7T4jGFmQ'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
