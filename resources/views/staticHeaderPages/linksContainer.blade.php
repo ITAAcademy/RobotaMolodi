d8d7e1b7ecffc0c102d00301c8876185e0827798
Vikarchuk <roma.vikarchuk@gmail.com>
2016-04-14 21:16:16 +0300

    Now, when user leave or update page he get message

--- resources/views/staticHeaderPages/linksContainer.blade.php
+++ resources/views/staticHeaderPages/linksContainer.blade.php
@@ -1,3 +1,3 @@
-<li><a href="aboutus" style="color: #f48952">Про нас</a></li>
-<li><a href="http://www.profitday.info" style="color: #f48952">Дні кар'єри</a></li>
-<li><a href="contacts" style="color: #f48952">Контакти</a></li>
+<li><a href="aboutus" class="afterChange" style="color: #f48952">Про нас</a></li>
+<li><a href="http://www.profitday.info" class="afterChange" style="color: #f48952">Дні кар'єри</a></li>
+<li><a href="contacts" style="color: #f48952" class="afterChange">Контакти</a></li>
