ae5dee7ede2e4240b073391b1a3748e13c9000c4
jc <jc@lori>
2016-04-16 11:16:57 +0300

    fix links
    414, 413, 419

--- resources/views/staticHeaderPages/linksContainer.blade.php
+++ resources/views/staticHeaderPages/linksContainer.blade.php
@@ -1,3 +1,3 @@
-<li><a href="aboutus" class="afterChange" style="color: #f48952">Про нас</a></li>
+<li><a href="{{ url('/aboutus') }}" class="afterChange" style="color: #f48952">Про нас</a></li>
 <li><a href="http://www.profitday.info" class="afterChange" style="color: #f48952">Дні кар'єри</a></li>
-<li><a href="contacts" style="color: #f48952" class="afterChange">Контакти</a></li>
+<li><a href="{{ url('/contacts') }}" style="color: #f48952" class="afterChange">Контакти</a></li>
