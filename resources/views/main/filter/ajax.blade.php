88d671b5d7577f7b5650c7b93d0aa93d0fb38bb2
Vikarchuk <roma.vikarchuk@gmail.com>
2016-06-04 09:51:41 +0300

    adding google map to vacancy filter, determine geolocation user

--- resources/views/main/filter/ajax.blade.php
+++ resources/views/main/filter/ajax.blade.php
@@ -7,8 +7,8 @@
 <title>Robota Molodi</title>
 <meta name="csrf_token" content="{{ csrf_token() }}" />
 {!! Form::open(['method' => 'get',  'class'=>'form-inline']) !!}
-<select name="city" class="js_drop_menu" id="selectCity" style="float: left; width: 27%">
-<option value="empty">Уся Україна</option>
+<select name="city" multiple id="selectCity" style="float: left; width: 27%">
+{{--<option value="empty">Уся Україна</option>--}}
     @foreach($cities as $city)
         <option value="{{$city->id}}"> {{$city->name}} </option>
     @endforeach
@@ -24,50 +24,24 @@
     @foreach($specialisation as $spec)
         <option value="{{$spec}}"> {{$spec}} </option>
     @endforeach
-
 </select>
+<div class="panel-group" id="collapse-group">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h4 class="panel-title">
+                <a data-toggle="collapse" id="collapseButton" data-parent="#collapse-group" data-target="#el1">Карта</a>
+            </h4>
+        </div>
+        <div id="el1" class="panel-collapse collapse">
+            <div class="panel-body">
+                <div id="map-canvas" style="height: 300px"></div>
+            </div>
+        </div>
+    </div>
+</div>
 {!!Form::close()!!}
 
 @yield('category')
-
-
-<?php //echo $posts->render(); ?>
-
-<script>
-
-//    $(window).on('hashchange', function() {
-//        if (window.location.hash) {
-//            var page = window.location.hash.replace('#', '');
-//            if (page == Number.NaN || page <= 0) {
-//                return false;
-//            } else {
-//                getPosts(page);
-//            }
-//        }
-//    });
-//    $(document).ready(function() {
-//        $(document).on('click','.pagination a' , function (e) {
-//            getPosts($(this).attr('href').split('page=')[1]);
-//            e.preventDefault();
-//        });
-//        /////////////////////////////////////
-//        $('#selectIndustry').change(function(){
-//            $("div.list-group").empty();
-//            var city_id = $('[name=city]').val();
-//            var industry_id = $('[name=industry]').val();
-//            var url = 'showVacancies';
-//            sendAjax(city_id,industry_id,url);
-//        });
-//        /////////////////////////////////
-//        $('#selectCity').change(function(){
-//            $("div.list-group").empty();
-//            var city_id = $('[name=city]').val();
-//            var industry_id = $('[name=industry]').val();
-//            var url = 'showVacancies';
-//            sendAjax(city_id,industry_id,url);
-//
-//        });
-//    });
-
-</script>
 @stop
+
+
