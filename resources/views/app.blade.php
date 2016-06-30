7d8292c2fd3322c80876d753d1a35c6c01e9e1ab
Vikarchuk <roma.vikarchuk@gmail.com>
2016-06-30 23:26:14 +0300

    correcting mistakes with filterVacancies and filterResumes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,20 +13,17 @@
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-     <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet" />
+    <!-- <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/css/select2.min.css" rel="stylesheet" /> -->
 
     <!-- geoFilter yandex links -->
     <script src="http://yastatic.net/jquery/2.1.1/jquery.min.js"></script>
     <script src="http://api-maps.yandex.ru/2.0-stable/?load=package.standard&lang=uk-UA" type="text/javascript"></script>
-	{{--<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>--}}
     <!-- end here -->
 
     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.js"></script>
     {!!Html::script('js/robotaMolodiUtils.js')!!}
 	{!!Html::script('js/formatDate.js')!!}
-	{!!Html::script('js/initMap.js')!!}
-
     {{--<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">--}}
     {{--<link href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" rel="stylesheet">--}}
 
@@ -46,9 +43,7 @@
 	<nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<div class="navbar-header">
-				<div class="text-center">
-                  <a href="{{ url('/') }}" class="afterChange">{!! Html::image('image/logo.png','Головна',['id'=>'logoImg']) !!} </a>
-				</div>
+
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 					<span class="sr-only">Toggle Navigation</span>
 					<span class="icon-bar"></span>
@@ -61,7 +56,7 @@
 
 				<ul class="nav navbar-nav navbar-right">
 
-					@include('staticHeaderPages.linksContainer')
+
 
 						<!--
 
@@ -71,27 +66,7 @@
 
 						</li>
 						<form method="POST" action="{{ url('searchResumes') }}"name="search_form"></form> -->
-						<li>@if(Request::is('sresume')){!!Form::open(['route' => 'searchResume','method' => 'POST'])!!}
-						@elseif(Request::is('searchResumes')){!!Form::open(['route' => 'searchResume','method' => 'POST'])!!}
-						@elseif(Request::is('scompany')){!!Form::open(['route' => 'searchCompany','method' => 'POST'])!!}
-						@elseif(Request::is('searchCompanies')){!!Form::open(['route' => 'searchCompany','method' => 'POST'])!!}
-						@else{!!Form::open(['route' => 'searchVacancy','method' => 'POST'])!!}
-						@endif</li>
-						<li>
-							<div class="nav navbar-nav navbar-right">{!! Form::text('search_field','',array( 'class' => 'form-control','placeholder' => 'Введіть запит' )) !!}</div>
-						</li>
 
-
-
-
-						<li>
-							<button type="submit" Style="background-color:gray;color:white;" class="navbar-right btn btn-default afterChange"onclick="
-							@if(Request::is('sresume')) window.location='{{ url('searchResumes') }}'
-							@else window.location='{{ url('searchVacancies') }}'
-							@endif">Пошук</button>
-
-						</li>
-						{!!Form::close()!!}
 					@if (Auth::guest())
 						<li><a href="{{ url('/auth/login') }}"><span>{!! Html::image('image/entry.png','Головна',['id'=>'entry']) !!}</span> Увійти</a></li>
 						<li><a href="{{ url('/auth/register') }}"><span>{!! Html::image('image/registry.png','Головна',['id'=>'registry']) !!}</span> Зареєструватись</a></li>
@@ -111,7 +86,32 @@
 			</div>
 		</div><!--class="container-fluid"-->
 	</nav>
+	<div class="col-md-offset-5" id="logoImg">
+		<a href="{{ url('/') }}" class="afterChange">{!! Html::image('image/logo.png','Головна') !!} </a>
+	</div>
+	<div class="row">
+		@if(Request::is('sresume')){!!Form::open(['route' => 'searchResume','method' => 'POST'])!!}
+		@elseif(Request::is('searchResumes')){!!Form::open(['route' => 'searchResume','method' => 'POST'])!!}
+		@elseif(Request::is('scompany')){!!Form::open(['route' => 'searchCompany','method' => 'POST'])!!}
+		@elseif(Request::is('searchCompanies')){!!Form::open(['route' => 'searchCompany','method' => 'POST'])!!}
+		@else{!!Form::open(['route' => 'searchVacancy','method' => 'POST','class' => 'span2'])!!}
+		@endif
+		<div class="col-md-10 col-md-offset-2" style="margin-top: 5px; margin-bottom: 5px;">
+			<span class="col-md-1 staticLinks"><a class="btn"  href="{{ url('/aboutus') }}" class="afterChange" style="color: #333333; font-size: 14px">Про нас</a> </span>
+			<span class="col-md-1 staticLinks"><a class="btn" href="#" onclick="window.open('http://www.profitday.info')" style="color: #333333">Дні кар'єри</a> </span>
+			<span class="col-md-1 staticLinks"><a class="btn" href="{{ url('/contacts') }}" style="color: #333333" class="afterChange">Контакти</a> </span>
+			<span class="col-md-4">
+				<button type="submit" Style="background-color:gray;color:white;" class="btn btn-default afterChange"onclick="
+				@if(Request::is('sresume')) window.location='{{ url('searchResumes') }}'
+				@else window.location='{{ url('searchVacancies') }}'
+				@endif">Пошук</button>
+			<span class="col-md-6"> {!! Form::text('search_field','',array( 'class' => 'form-control','placeholder' => 'Введіть запит' )) !!} </span>
 
+				{!!Form::close()!!}
+
+			</span>
+		</div>
+	</div>
         <div class="row">
             <div class="col-md-10 col-md-offset-1" >
                 <div class="col-md-10 col-md-offset-1" >
@@ -151,15 +151,10 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 
-	<script src="http://yastatic.net/jquery/2.1.1/jquery.min.js"></script>
-	<script src="http://api-maps.yandex.ru/2.0-stable/?load=package.standard&lang=uk-UA" type="text/javascript"></script>
-	{{--<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>--}}
+
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
-		{{--{!!Html::script('js/select2.full.js')!!}--}}
-
-
-
+    <!-- <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script> -->
+		{!!Html::script('js/select2.full.js')!!}
 
 <script type="text/javascript">
 $(document).ready(function() {
@@ -168,11 +163,11 @@ $(document).ready(function() {
 	  tags: []
 //		tokenSeparators: [",", " "]
   });
-//	$("#e12").select2({tags:[]});
+	//$("#e12").select2({tags:[]});
 
-//  $(".js_drop_menu").on("change", function(e){
-//	  console.dir(e);
-//  })
+  //$(".js_drop_menu").on("change", function(e){
+	  //console.dir(e);
+  //})
 });
 
 
