23c48b0616650bc8f08cee4dcf5e851253ad3604
stoha27 <stoha27@gmail.com>
2017-01-21 00:00:31 +0200

    change contact

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -107,26 +107,19 @@
 		</div>
 		{{--<div class="col-md-10 col-md-offset-2 rovn">--}}
 		<div class="col-md-12 col-search" >
-
-
-			{{--<span class="paddlft">--}}
 			{!!Form::text('search_field','',array( 'class' => 'form-control','placeholder' => 'Введіть запит' )) !!}
 			{!!Form::close()!!}
-
-			{{--</span>--}}
-			{{--<span class="paddlft">--}}
 			<input type="submit" class="btn btn-default btn-search navbar-default" onclick="
 			@if(Request::is('sresume')) window.location='{{ url('searchResumes') }}'
 			@else window.location='{{ url('searchVacancies') }}'
 			@endif" value="Пошук">
-			</input>
-			{{--</span>--}}
 		</div>
-		{{--</div>--}}
 	</div>
 	<div class="row">
 		{{--<div class="col-md-10 col-md-offset-1" >--}}{{-- Stoha--}}
-		<div class="col-md-10 col-md-offset-1" > {{-- Stoha --}}
+		<div class="col-md-10 col-md-offset-1" >
+			@yield('search')
+			{{-- Stoha --}}
 			<div class="panel-heading" style="background-color: #ffffff;">
 				<div class="row">
 					<div>
