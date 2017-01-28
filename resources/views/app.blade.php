8e7d2566c56a0831fec2a33a09a6eb765a836b10
stoha27 <stoha27@gmail.com>
2017-01-28 14:09:03 +0200

    remove main content into separate blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,65 +35,20 @@
 <body>
 	<div class="container-fluid container-main">
 		@include('header/header')
-		<div class="main">
-		<div class="col-md-10 col-md-offset-2 menu">
-			<span class="col-md-1 staticLinks"><a class="btn"  href="{{ url('/about/index.html') }}" class="afterChange" style="color: #333333; font-size: 14px">Про нас</a> </span>
-			<span class="col-md-1 staticLinks"><a class="btn" href="{{ url('http://www.profitday.info')}}" class="afterChange" style="color: #333333">Дні кар'єри</a> </span>
-			<span class="col-md-1 staticLinks"><a class="btn" href="{{ url('/contacts') }}" class="afterChange" style="color: #333333">Контакти</a> </span>
-		</div>
-			@if(Request::is('sresume')){!!Form::open(['route' => 'searchResume','method' => 'POST'])!!}
-			@elseif(Request::is('searchResumes')){!!Form::open(['route' => 'searchResume','method' => 'POST'])!!}
-			@elseif(Request::is('scompany')){!!Form::open(['route' => 'searchCompany','method' => 'POST'])!!}
-			@elseif(Request::is('searchCompanies')){!!Form::open(['route' => 'searchCompany','method' => 'POST'])!!}
-			@else{!!Form::open(['route' => 'searchVacancy','method' => 'POST','class' => 'span2'])!!}
-			@endif
-
-
-			<div class="col-search" >
-				{!!Form::text('search_field','',array( 'class' => 'form-control','placeholder' => 'Введіть запит' )) !!}
-				{!!Form::close()!!}
-				<input type="submit" class="btn btn-default btn-search navbar-default" onclick="
-				@if(Request::is('sresume')) window.location='{{ url('searchResumes') }}'
-				@else window.location='{{ url('searchVacancies') }}'
-				@endif" value="Пошук">
-			</div>
-
-		<div class="row">
-			<div class="col-md-12" >
-				<div class="panel-heading" style="background-color: #ffffff;">
-					<div class="row">
-						<div>
-							@yield ('btn')
-						</div>
-						<div id="dropTitle">
-							@yield('title')
-						</div>
-
-						<div class="crResVac">
-							@yield('Create_res_vac')
-						</div>
-					</div>
-				</div>
-				<div >
-					@yield('content')
-				</div>
-			</div>
-		</div>
+		@include('main/mainContent')
 	</div>
-	</div>
-		<footer>
-			@include('footer/footer')
-		</footer>
+	<footer>
+		@include('footer/footer')
+	</footer>
 
-		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-    	<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<!-- <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script> -->
-		{!!Html::script('js/select2.full.js')!!}
+	<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	{!!Html::script('js/select2.full.js')!!}
 
 		<script type="text/javascript">
 			$(document).ready(function() {
@@ -108,8 +63,6 @@
 				//console.dir(e);
 				//})
 			});
-
-
 		</script>
 
 		<script>
@@ -129,5 +82,4 @@
 			ga('send', 'pageview');
 		</script>
 </body>
-
 </html>
---
+++ resources/views/main/mainContent.blade.php
@@ -0,0 +1,45 @@
+<div class="main">
+    <div class="col-md-10 col-md-offset-2 menu">
+        <span class="col-md-1 staticLinks"><a class="btn"  href="{{ url('/about/index.html') }}" class="afterChange" style="color: #333333; font-size: 14px">Про нас</a> </span>
+        <span class="col-md-1 staticLinks"><a class="btn" href="{{ url('http://www.profitday.info')}}" class="afterChange" style="color: #333333">Дні кар'єри</a> </span>
+        <span class="col-md-1 staticLinks"><a class="btn" href="{{ url('/contacts') }}" class="afterChange" style="color: #333333">Контакти</a> </span>
+    </div>
+    @if(Request::is('sresume')){!!Form::open(['route' => 'searchResume','method' => 'POST'])!!}
+    @elseif(Request::is('searchResumes')){!!Form::open(['route' => 'searchResume','method' => 'POST'])!!}
+    @elseif(Request::is('scompany')){!!Form::open(['route' => 'searchCompany','method' => 'POST'])!!}
+    @elseif(Request::is('searchCompanies')){!!Form::open(['route' => 'searchCompany','method' => 'POST'])!!}
+    @else{!!Form::open(['route' => 'searchVacancy','method' => 'POST','class' => 'span2'])!!}
+    @endif
+
+
+    <div class="col-search" >
+        {!!Form::text('search_field','',array( 'class' => 'form-control','placeholder' => 'Введіть запит' )) !!}
+        {!!Form::close()!!}
+        <input type="submit" class="btn btn-default btn-search navbar-default" onclick="
+        @if(Request::is('sresume')) window.location='{{ url('searchResumes') }}'
+        @else window.location='{{ url('searchVacancies') }}'
+        @endif" value="Пошук">
+    </div>
+
+    <div class="row">
+        <div class="col-md-12" >
+            <div class="panel-heading" style="background-color: #ffffff;">
+                <div class="row">
+                    <div>
+                        @yield ('btn')
+                    </div>
+                    <div id="dropTitle">
+                        @yield('title')
+                    </div>
+
+                    <div class="crResVac">
+                        @yield('Create_res_vac')
+                    </div>
+                </div>
+            </div>
+            <div >
+                @yield('content')
+            </div>
+        </div>
+    </div>
+</div>
