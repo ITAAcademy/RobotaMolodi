cd15ff55f95c3781cd499ae2781dc90c4db55edc
BodikSilver <bodik92.ua@gmail.com>
2017-02-19 17:03:13 +0200

    refactor sortAds script link

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -40,7 +40,7 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="http://yastatic.net/jquery/2.1.1/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script src="js/sortAds/tcal.js"></script>
+	{!! Html::script('js/sortAds/tcal.js') !!}
 	<!-- EndSort -->
 
 	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
