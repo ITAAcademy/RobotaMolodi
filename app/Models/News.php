5377262be8d92bbce3218735debba039b6026f36
kyrychenkoV <keryvn@gmail.com>
2017-03-09 12:54:08 +0200

    form validation in the controller

--- app/Models/News.php
+++ app/Models/News.php
@@ -6,14 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    public function createModel($request,$pictureName)
     {
-        $patch='image/uploads/news/';
-        $news=new News;
-        $news->name=$request->input('title');
-        $news->description=$request->input('description');
-        $news->img=$patch.$pictureName;
-        $news->published='1';
         $news->save();
 
     }
