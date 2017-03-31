0411eb10adf24b08fc133baa868d09df6f8e1a23
BogdanTaranenko <bogdan13taranenko@gmail.com>
2017-03-31 20:09:07 +0300

    Fix wrong url for creating vacancy

--- resources/views/cabinet/cabinet.blade.php
+++ resources/views/cabinet/cabinet.blade.php
@@ -1,16 +1,6 @@
 @extends('app')
 @section('title')
 
-    <div class="panel panel-default">
-
-        <div class="panel-body" style="padding: inherit;position: absolute; margin-top: -5px;" >
-            <ul class="nav nav-tabs">
-
-            </ul>
-
-        </div>
-    </div>
-@stop
 @section('content')
     <div class="row">
         <div class="col-xs-11 col-md-7 header-tabs">
