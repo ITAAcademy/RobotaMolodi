401aa5472de5c578e8598ed14c69449dc627cc2b
Unknown <bogdan13taranenko@gmail.com>
2017-04-08 13:16:44 +0300

    New look of my resumes, vacancies, companies.
    colors, margin already fixed. 
    Also, added name of vacancy and time on page "My vacancy",

--- resources/views/vacancy/_vacancy.blade.php
+++ resources/views/vacancy/_vacancy.blade.php
@@ -1,20 +1,32 @@
 {!! $vacancies->render(new App\Presenters\BootstrapTwoPresenter($vacancies)) !!}
     @foreach($vacancies as $vacancy)
     <article>
-        <a href="/vacancy/{{$vacancy->id}}" class="link">
-            <div class="list">
+        <div class="list">
+            <a href="/vacancy/{{$vacancy->id}}" class="link">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-
+                        <span class="text-info" style="color: #555555; font-size: 16px">
+                            {{$vacancy->position}}
+                        </span>
+                        <span class="vacancy-salary" style="font-size: 16px">
+                            &#183;{{$vacancy->salary}} - {{$vacancy->salary_max}} {{$vacancy->Currency()[0]['currency']}}
+                        </span>
+                        <span class="text-muted text-right pull-right">
+                            <h5 id="{{$vacancy->id}}" title="{{ date('j.m.Y, H:i:s', strtotime($vacancy->updated_at))}}">
+                                <script>
+                                    $('#'+'{{$vacancy->id}}').text(FormatDate({{strtotime($vacancy->updated_at)}}));
+                                </script>
+                            </h5>
+                        </span>
                     </div>
                     <div class="panel-body">
-                        <h4 class="list-group-item-heading">{{ $vacancy->Industry()->name}}</h4>
-                        <h4 class="list-group-item-heading">{{ $vacancy->Company()->company_name}}</h4>
-                        <h4 class="list-group-item-heading">@foreach($vacancy->City() as $city){{ $city->name}} @endforeach</h4>
+                        <h4 class="list-group-item-heading" style="color: #555555">{{ $vacancy->Industry()->name}}</h4>
+                        <h4 class="list-group-item-heading" style="color: #555555">{{ $vacancy->Company()->company_name}}</h4>
+                        <h4 class="list-group-item-heading" style="color: #555555">@foreach($vacancy->City() as $city){{ $city->name}} @endforeach</h4>
                     </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
     </article>
     @endforeach
 {!! $vacancies->render(new App\Presenters\BootstrapTwoPresenter($vacancies)) !!}
