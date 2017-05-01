4e9d417b63b1660ed74102b95411b7a531eaf2db
garik <garik@mail.com>
2017-05-01 20:36:22 +0300

    fixed and changed news part

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -5,104 +5,66 @@ namespace App\Http\Controllers;
 use App\Models\City;
 use App\Models\Industry;
 use App\Models\Vacancy;
-use Illuminate\Support\Facades\Session;
-use Illuminate\Http\Request;
 
-use App\Http\Requests;
 use App\Models\News;
+
+use Request;
+
 
 class NewsController extends Controller
 {
-//    const DELETE='delete object';
 
-//    public function index()
-//    {
-//        $news = News::all();
-//        return view('newDesign.news.index', ['news' => $news]);
-//    }
-//
-//    public function create()
-//    {
-//        return view('newDesign.news.form');
-//    }
-//
-//    public function store(Request $request)
-//    {
-//        $news = new News;
-//        if ($news->validateForm($request->all())) {
-//            $this->helperSave($news,$request);
-//            Session::flash('flash_message', 'news successfully created!');
-//            return redirect()->route('news.index');
-//        } else {
-//            return redirect()->route('news.create')->withInput()->withErrors($news->getErrorsMessages());
-//        }
-//    }
-//
-//    public function show($id)
-//    {
-//        $newsOne = News::find($id);
-//        return view('newDesign.news.one', ['newsOne' => $newsOne]);
-//    }
-//
-//    public function edit($id)
-//    {
-//        $newsOne = News::find($id);
-//        return view('newDesign.news.edit', ['newsOne' => $newsOne]);
-//    }
-//
-//    public function update(Request $request, $id)
-//    {
-//        /**
-//         * @var News $newsOne
-//         */
-//        $newsOne = News::find($id);
-//        if ($newsOne->validateForm($request->all())) {
-//            $this->helperSave($newsOne,$request);
-//            Session::flash('flash_message', 'news successfully added!');
-//            return redirect()->route('news.index');
-//        } else {
-//            return redirect()->route('news.edit', ['newsOne' => $newsOne])->withInput()->withErrors($newsOne->getErrorsMessages());
-//
-//        }
-//    }
-//
-//    public function destroy($id)
-//    {
-//        /**
-//         * @var News $news
-//         */
-//        $news = News::find($id);
-//        $news->deleteNews();
-//        Session::flash('flash_message', 'news successfully deleted!');
-//        return redirect()->route('news.index');
-//    }
-    private function helperSave($news,$request){
-        $news->saveImage($request);
-        $input = $request->all();
-        $news->fill($input)->save();
-    }
-
-    public function showListNews()
+    public function index()
     {
+        $news = News::latest('updated_at');
+
+        if(Request::ajax()){
+            return view('newDesign.news.includeNews.newsListContent', array(
+                'newsPagin' => $news->paginate(4)
+            ));
+        }
         $topVacancy = Vacancy::bySort('desc')->take(5)->get();
         return view('newDesign.news.includeNews.newsList', [
-            'news'=>(new News())->getNewsForMainPage(),
+            'news'=> $news->get(),
+            'newsPagin' => $news->paginate(4),
             'cities' => City::all(),
             'industries' => Industry::all(),
             'topVacancy' => $topVacancy,
         ]);
     }
-    public function showPageNews($id)
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show($id)
     {
         $newsOne = News::find($id);
         $topVacancy = Vacancy::bySort('desc')->take(5)->get();
+
         return view('newDesign.news.includeNews.newsPage', [
-            'news'=>(new News())->getNewsForMainPage(),
+            'news'=> News::latest('updated_at')->get(),
             'newsOne' => $newsOne,
             'cities' => City::all(),
             'industries' => Industry::all(),
             'topVacancy' => $topVacancy,
         ]);
+    }
+
+    public function edit($id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function destroy($id)
+    {
     }
 
 }
