ad76f75c9d52ced138a11dfa98f7061523cfd0e7
Magnum888 <K.O.M.888@i.ua>
2017-05-13 17:59:49 +0300

    added new design myVacancy

--- resources/views/vacancy/showMyVacancy.blade.php
+++ resources/views/vacancy/showMyVacancy.blade.php
@@ -1,19 +1,18 @@
 @extends('app')
-
+<link href="{{ asset('/css/myVacancyShow.css') }}" rel="stylesheet">
 @section('content')
     {!!Form::open(['route' => 'head', 'method' => 'post', 'name' => 'filthForm', 'id' => 'aform'])!!}
     <input type = "hidden" name = "filterName" id = "filterName"/>
     <input type = "hidden" name = "filterValue" id = "filterValue"/>
     {!!Form::close()!!}
-    {!! Form::open(array('route' => 'upimg', 'files' => true, 'style' => 'display: none', 'name' => 'uploadImgForm')) !!}
-    <input type="file" name="image" id="fileImg">
-    <input type="hidden" name="rov" value="v">
-    <input type="hidden" name="fname" value="{{$vacancy->company_id}}">
-    {!! Form::close() !!}
 
-    <div class="panel panel-orange" id="vrBlock">
+    {!! Form::file('fileImg', array( 'id'=>'fileImg', 'style'=>'display:none', 'accept'=>'.jpg, .jpeg, .gif, .png, .svg')) !!}
+    <input type="hidden" name="fcoords" id="coords" class="coords" value="" data-id="{{$vacancy->company_id}}">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-        <div class="logos">
+    <div class="panel" id="vrBlock">
+    <div class="row">
+        <div class="col-xs-12 col-md-2">
             <div class="panel panel-orange" id="vimg">
                 @if(File::exists(public_path('image/company/' . $vacancy->Company->users_id .'/'. $vacancy->Company->image)) and $vacancy->Company->image != '')
                     {!! Html::image('image/company/' . $vacancy->Company->users_id .'/'. $vacancy->Company->image, 'logo', ['id' => 'vacImg', 'width' => '100%', 'height' => '100%']) !!}
@@ -21,56 +20,140 @@
                     <h3 style="text-align: center; color: #f48952; margin-top: 40px">логотип вiдсутнiй</h3>
                 @endif
             </div>
-
-            <a style="margin-left: 15px" class="orangeLinks" href="javascript:sendFile()">
-                {!! Html::image('image/update.png', 'del') !!}
-                <span style="font-size: 14px; text-decoration: none">Змiнити лого</span>
-            </a>
+            {{--<div class="change-img-myVacancy">--}}
+                {{--<div class="orange-link-myVacancy"  id="changeImage">--}}
+                    {{--<span class="glyphicon glyphicon-repeat" aria-hidden="true"></span>--}}
+                    {{--<span>Змiнити лого</span>--}}
+                {{--</div>--}}
+                {{--<br>--}}
+                {{--@if(File::exists(public_path('image/company/' . $vacancy->Company->users_id .'/'. $vacancy->Company->image)) and $vacancy->Company->image != '')--}}
+                    {{--<span class="orange-link-myVacancy" id="deleteImage">--}}
+                            {{--<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>--}}
+                            {{--<span>Видалити лого</span>--}}
+                    {{--</span>--}}
+                {{--@endif--}}
+            {{--</div>--}}
         </div>
-
-        <div id="datAnnoyingSizes">
-            <div class="panel-heading">
-                <h2>
-                    <a class="orangeLinks" href="javascript:submit('selectSpecialisation', '{{$vacancy->position}}')">{{$vacancy->position}}</a>
-                        <br><span style="color: red">{{$vacancy->salary}} - {{$vacancy->salary_max}} {{$vacancy->Currency()[0]['currency']}}</span>
-                </h2>
+        <div class="col-xs-12 col-md-10">
+            <div>
+                <p class="position-myVacancy">
+                    <a class="orangColor-myVacancy-name" href="javascript:submit('selectSpecialisation', '{{$vacancy->position}}')">{!!$vacancy->position!!}</a>
+                    <br>
+                </p>
+                <p class="price-myVacancy">
+                    <span>{{$vacancy->salary}} - {{$vacancy->salary_max}} {{$vacancy->Currency()[0]['currency']}}</span>
+                </p>
             </div>
-            <ul class="list-group">
-                <li class="list-group-item">{{auth()->user()->name}}</li>
-                <li class="list-group-item">  <a class="orangeLinks" href="javascript:submit('selectIndustry', {{$vacancy->Industry()->id}})">{{$industry->name}}</a></li>
-                <li class="list-group-item" style="border-bottom: none"><span class="heading">Опис: </span>{{$vacancy->description}}</li>
-                <li class="list-group-item">  <a style="text-decoration: underline" target="_blank" class="orangeLinks" href="/company/{{$company->id}}">{{$company->company_name}}</a>
-                <li class="list-group-item" id="opt-data-low" style="color: #777777;">@foreach($cities as $city)<a class="orangeLinks" href="javascript:submit('selectCity' {{$city->id}})">{{$city->name}} </a>@endforeach<span id="yellowCircle">&#183;</span> {{ date('j.m.Y, H:i:s', strtotime($vacancy->updated_at))}}</li>
-            </ul>
+            <div>
+                <p class="position-myVacancy">  <a class="orangColor-myVacancy" href="javascript:submit('selectIndustry', {{$vacancy->Industry()->id}})">{{$industry->name}}</a></p>
+                {{--<p class="company-name-myVacancy">{{auth()->user()->name}}</p>--}}
+            </div>
+            <div>
+                <p class="description-footer-myVacancy">{!! strip_tags($vacancy->description, '<em><a><s><p><span><b><ul><ol><li><strong><h1><h2><h3><h4><h5><blockquote><body><table><tr><td>') !!}</p>
+            </div>
+            <div>
+                <a target="_blank" class="nameCompany-myVacancy" href="/company/{{$company->id}}">{{$company->company_name}}</a>
+            </div>
+            <div>
+                <p class="cityTime-myVacancy">
+                    @foreach($cities->get() as $city)
+                        <a class="city-myVacancy" href="javascript:submit('selectCity' {{$city->id}})">{{$city->name}} </a>
+                    @endforeach
+                    <span id="yellowCircle-myVacancy">&#183;</span> {{ date('j m Y', strtotime($vacancy->updated_at))}}
+                </p>
+            </div>
         </div>
-
+    </div>
     </div>
 
-    <ul class="nav navbar-nav navbar-left" style="margin-left: 225px">
-        <li>
-            <a class="orangeLinks" href="{{$vacancy->id}}/destroy" onclick="return ConfirmDelete();">
-                {!! Html::image('image/delete.png', 'del') !!}
-                <span style="text-decoration: underline">Видалити</span>
-            </a>
-        </li>
-        <li>
-            <a class="orangeLinks" href="{{$vacancy->id}}/edit">
-                {!! Html::image('image/edit.png', 'del') !!}
-                <span style="text-decoration: underline">Редагувати</span>
-            </a>
-        </li>
-    </ul>
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">
+            <div class="col-xs-12 col-md-3">
+                <a class="orangColor-myVacancy" href="{{$vacancy->id}}/destroy" onclick="return ConfirmDelete();">
+                    <i class="fa fa-trash" aria-hidden="true"></i>
+                    <span>видалити</span>
+                </a>
+            </div>
+            <div class="col-xs-12 col-md-3">
+                <a class="orangColor-myVacancy" href="{{$vacancy->id}}/edit">
+                    {!! Html::image('image/edit.png', 'del') !!}
+                    <span>редагувати</span>
+                </a>
+            </div>
+            <div class="col-xs-12 col-md-3">
+                <a class="orangColor-myVacancy" href="#">
+                    {!! Html::image('image/podiumOrenge.png', 'del') !!}
+                    <span> розмістити в ТОПі</span>
+                </a>
+            </div>
+            <div class="col-xs-12 col-md-3">
+                <a class="orangColor-myVacancy" href="#">
+                    <i class="fa fa-calendar" aria-hidden="true"></i>
+                    <span>оновити дату вакансіїї</span>
+                </a>
+            </div>
+        </div>
+    </div>
 
+    <div id="changeImageBox" class="modal fade">
+        @include('newDesign.cropModal')
+    </div>
+
+    {!!Html::script('js/crop.js')!!}
     <script>
-        document.getElementById("fileImg").onchange = function()
-        {
-            document.uploadImgForm.submit();
-        };
-        function sendFile()
-        {
-            var input = document.getElementById('fileImg');
-            input.click();
-        }
+        $(document).ready(function () {
+            $('#changeImage').on('click', function () {
+                $('#fileImg').click();
+            });
+
+            $('#fileImg').on('change', function (e) {
+                $('#changeImageBox').modal({
+                    show: true,
+                    backdrop: 'static'
+                });
+                crop(e, 'img-src', '#crop', '#changeImageBox');
+            });
+
+            $('#changeImageBox').on('hidden.bs.modal', function () {
+                if($('#coords').val()){
+                    var $input = $("#fileImg");
+                    var fd = new FormData;
+                    fd.append('fileImg', $input.prop('files')[0]);
+                    fd.append('coords', $('.coords').val());
+                    fd.append('id', $('.coords').attr('data-id'));
+                    $.ajax({
+                        url: '{{ route('upimg') }}',
+                        data: fd,
+                        processData: false,
+                        contentType: false,
+                        type: 'POST',
+                        success: function (data) {
+                            $('#vimg img').attr('src', window.location.origin + '/' + data);
+                        }
+                    });
+                }
+            });
+
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('input[name="_token"]').val()
+                }
+            });
+
+            $('#deleteImage').on('click', function () {
+                if(ConfirmDelete()){
+                    $.ajax({
+                        url: '{{ route('deleteimg') }}',
+                        data: {'id' : $('.coords').attr('data-id')},
+                        type: 'POST',
+                        success: function (data) {
+                            $('#vimg img').attr('src', window.location.origin + '/' + data);
+                        }
+                    })
+                }
+            })
+        });
 
         function ConfirmDelete()
         {
