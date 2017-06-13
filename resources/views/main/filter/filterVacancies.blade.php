b4db5a3f481b66769cbca59501c8d521a10caa1c
magnum <k.o.m.888@i.ua>
2017-06-13 22:49:08 +0300

    fix mobile section sort

--- resources/views/main/filter/filterVacancies.blade.php
+++ resources/views/main/filter/filterVacancies.blade.php
@@ -5,12 +5,12 @@
     @include('newDesign/aboutUs/show')
     @include('newDesign/navTab/navTab')
     @include('newDesign/search/show')
-    <div id="left-content-column" class="col-xs-9">
+    <div id="left-content-column" class="col-sm-9">
         @include('newDesign/sortAds/sort')
         @include('newDesign/vacancies/vacanciesList')
         @include('newDesign/sliders/byCategory', ['viewName' => 'underFooter', 'category' => 1])
     </div>
-    <div id="right-content-column" class="col-xs-3">
+    <div id="right-content-column" class="col-sm-3">
         @include('newDesign/vacancies/topVacancies')
         @include('newDesign/sliders/byCategory', ['viewName' => 'news', 'category' => 2])
         @include('newDesign/topNews')
