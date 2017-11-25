af78cfe1b619fb15150c120afc458ea48a79dc7e
Olexii Kolomiiets <alexkolomiits@gmail.com>
2017-11-25 22:47:55 +0200

    delete empty feild in view

--- resources/views/newDesign/news/includeNews/newsPage.blade.php
+++ resources/views/newDesign/news/includeNews/newsPage.blade.php
@@ -8,7 +8,7 @@
         <div class="newsPageBreadcrumb">
             <ol class="breadcrumb">
                 <li><a href="/">Головна</a></li>
-                <li><a href="/news">Новини</a> </li>
+                <li><a href="/news">Новини</a></li>
                 <li class="active">{{date('j.m.Y', strtotime($newsOne->updated_at))}} </li>
             </ol>
         </div>
@@ -20,12 +20,11 @@
             <div id="left-content-column" class="col-xs-9">
                 <h3 class="nameNews">{{ $newsOne->name}}</h3>
                 @if($newsOne->img!='Not picture')
-                    <div><img class="picture" src="{{ asset($newsOne->getPath().$newsOne->img) }}" ></div>
+                    <div><img class="picture" src="{{ asset($newsOne->getPath().$newsOne->img) }}"></div>
                 @endif
                 <p>{!! $newsOne->description !!}</p>
                 <span><h4>Опубліковано </h4>{{date('j.m.Y', strtotime($newsOne->updated_at))}}</span>
                 <p>
-
                     @if($newsOne->id > 1)
                         {!! Html::linkRoute('news.show', '&laquo; Previous',  ['id' => $newsOne->id - 1],[ 'class' => 'previous pull-left']) !!}
                     @endif
