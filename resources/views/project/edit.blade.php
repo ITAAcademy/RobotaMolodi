04730c76616a6ce38a84ab9bc7854d3882c0376a
misha3674 <maydanskiy.misha@gmail.com>
2017-12-09 12:50:52 +0000

    displaying vacancies in project

--- resources/views/project/edit.blade.php
+++ resources/views/project/edit.blade.php
@@ -3,7 +3,7 @@
 <h1 class="text-center">{{ trans('project.editProject')}} </h1>
 <div class="container">
     <div class="row">
-        <div class="col-xs-8 col-xs-offset-2">
+        <div class="col-xs-10">
             @if ($errors->any())
                 <div class="alert alert-danger">
                     <ul>
