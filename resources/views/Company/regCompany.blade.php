6c432950ac8e14684a3496be73c3cf7bb363ea59
Olexii Kolomiiets <alexkolomiits@gmail.com>
2017-12-19 22:47:41 +0200

    added script for displaying selected image

--- resources/views/Company/regCompany.blade.php
+++ resources/views/Company/regCompany.blade.php
@@ -123,6 +123,20 @@
 
             <script>
                 $(document).ready(function () {
+                    function readURL(input) {
+                        if (input.files && input.files[0]) {
+                            var reader = new FileReader();
+
+                            reader.onload = function(e) {
+                                $('.blah').attr('src', e.target.result);
+                            };
+
+                            reader.readAsDataURL(input.files[0]);
+                        }
+                    }
+                    $(".imgInp").change(function() {
+                        readURL(this);
+                    });
                     $('#loadCompany').on('change', function(e) {
                         $('#imageBox').modal({
                             show: true,
