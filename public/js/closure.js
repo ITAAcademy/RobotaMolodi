23c1c9c2bc89f35fda0a800435e776549c5c6db4
zakat <zakat@MBP-Iurii.Dlink>
2018-01-11 14:53:53 +0200

    closure script has been changed, it adds display property "none" to element with class=unavailable now

--- public/js/closure.js
+++ public/js/closure.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-    $('.unavailable').on('click',function(event){
-        event.preventDefault();
+    // $('.unavailable').on('click',function(event){
+    //     event.preventDefault();
+    //
+    //     $.ajax({
+    //         url: '/unavailable',
+    //         success:
+    //             function(response){
+    //                 $('#modal-common .modal-content').html(response);
+    //                 $('#modal-common').modal('show');
+    //             }
+    //     });
+    // });
 
-        $.ajax({
-            url: '/unavailable',
-            success:
-                function(response){
-                    $('#modal-common .modal-content').html(response);
-                    $('#modal-common').modal('show');
-                }
-        });
-    });
+    $('.unavailable').hide();
 });
