41639f597ae9a469edec8675cfc33e7cc6c76726
Olexii Kolomiiets <alexkolomiits@gmail.com>
2018-01-16 22:35:54 +0200

    code style

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,6 +25,7 @@
     <link href="{{ asset('/css/errors/base.css') }}" rel="stylesheet">
 
 	@yield('head')
+
 	<link rel="shortcut icon" href="{{ asset('/image/favicon.png') }}" type="image/png">
 
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -89,30 +90,30 @@
         });
 	</script>
 
-		<script type="text/javascript">
-			$(document).ready(function() {
-				$(".js_drop_menu").select2()
-				$("#position").select2({
-					tags: []
-				});
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$(".js_drop_menu").select2()
+			$("#position").select2({
+				tags: []
 			});
-		</script>
+		});
+	</script>
 
-		<script>
-			(function (i, s, o, g, r, a, m) {
-				i['GoogleAnalyticsObject'] = r;
-				i[r] = i[r] || function () {
-							(i[r].q = i[r].q || []).push(arguments)
-						}, i[r].l = 1 * new Date();
-				a = s.createElement(o),
-						m = s.getElementsByTagName(o)[0];
-				a.async = 1;
-				a.src = g;
-				m.parentNode.insertBefore(a, m)
-			})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+	<script>
+		(function (i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r;
+			i[r] = i[r] || function () {
+						(i[r].q = i[r].q || []).push(arguments)
+					}, i[r].l = 1 * new Date();
+			a = s.createElement(o),
+					m = s.getElementsByTagName(o)[0];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore(a, m)
+		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-			ga('create', 'UA-83807118-1', 'auto');
-			ga('send', 'pageview');
-		</script>
+		ga('create', 'UA-83807118-1', 'auto');
+		ga('send', 'pageview');
+	</script>
 </body>
 </html>
