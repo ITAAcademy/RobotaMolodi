d4203c5a1f65eca081960fce472f5384abcae839
zakat <zakat@MBP-Iurii.Dlink>
2018-02-08 21:16:39 +0200

    finally fixed encrypt exception issue, changed closure script to catch and place service unavailable notification, one more uncached /place2top/ button found

--- public/js/closure.js
+++ public/js/closure.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-    // $('.unavailable').on('click',function(event){
-    //     event.preventDefault();
-    //
-    //     $.ajax({
-    //         url: '/unavailable',
-    //         success:
-    //             function(response){
-    //                 $('#modal-common .modal-content').html(response);
-    //                 $('#modal-common').modal('show');
-    //             }
-    //     });
-    // });
+    $('a[href$="unavailable"]').click(function(event){
+        event.preventDefault();
+
+        $.ajax({
+            url: '/unavailable',
+            success:
+                function(response){
+                    $('#modal-common .modal-content').html(response);
+                    $('#modal-common').modal('show');
+                }
+        });
+    });
 
     $('.unavailable').hide();
 });
