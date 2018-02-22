49de64e0be4f8b84a85fab9bc1f868efd12567cd
zakat <zakat@MBP-Iurii.Dlink>
2018-02-22 17:36:34 +0200

    changed clearing logic

--- resources/views/newDesign/seoModule/_meta.blade.php
+++ resources/views/newDesign/seoModule/_meta.blade.php
@@ -1,4 +1,4 @@
 <meta
         name="{{ $name }}"
-        content="{{substr(trim(preg_replace('/ +/', ' ',preg_replace('/[^A-Za-z0-9 ]/', ' ',urldecode(html_entity_decode(strip_tags($content)))))),0,150).'...' }}"
+        content="{{substr(trim(preg_replace('/ +/', ' ',preg_replace('/[^A-Za-z0-9 ]/', ' ',urldecode(html_entity_decode(strip_tags($content)))))),0,127).'...' }}"
 >
--- resources/views/newDesign/socialModule/meta.blade.php
+++ resources/views/newDesign/socialModule/meta.blade.php
@@ -1,7 +1,7 @@
 @include('newDesign.socialModule._index',[
     'title'         => (isset($title)) ? $title : 'Robota molodi',
     'description'   => (isset($description))
-                                            ? substr(preg_replace("/[^a-zA-Z0-9_ -]/s", "", strip_tags($description)),0,127).'...'
+                                            ? substr(trim(preg_replace('/ +/', ' ',preg_replace('/[^A-Za-z0-9 ]/', ' ',urldecode(html_entity_decode(strip_tags($description)))))),0,127).'...'
                                             : 'Project Robota Molodi created to simplify communication between young specialists and employers',
     'image'         => (isset($image) && $image!=NULL && $image != 'NULL' ) ? $image : '/image/logo.png',
     'url'           => URL::current(),
