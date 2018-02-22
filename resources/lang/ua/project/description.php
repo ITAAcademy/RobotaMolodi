f9c6edee77c4d0a1f677317a1f1197fc3dd27321
Olexii Kolomiiets <alexkolomiits@gmail.com>
2018-02-22 21:29:18 +0200

    deleted textareas; separated localization filse;

---
+++ resources/lang/ua/project/description.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    "name" => "Назва проекту",
+    "logo" => "Логотип",
+    "company" => "Компанія",
+    "industry" => "Галузь",
+    "companyDesc" => "Опис компанії",
+    "companyAbout" => "Про компанію",
+    "projectAbout" => "Про проект",
+    "projectTerm" => "Строк проекту",
+    "brand" => "Бренд",
+    "location" => "Місце знаходження",
+    "bonuses" => "Бонуси",
+];
