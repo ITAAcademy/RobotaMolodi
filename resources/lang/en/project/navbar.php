fbc830b89f10f6362b72febca2cb4355978e9283
Olexii Kolomiiets <alexkolomiits@gmail.com>
2018-02-23 21:58:36 +0200

    added anchors for navigation buttons

--- resources/lang/en/project/navbar.php
+++ resources/lang/en/project/navbar.php
@@ -2,7 +2,7 @@
 return [
     'about_company' => 'About Company',
     'about_project' => 'About Project',
-    'acancies_project' => 'Vacancies for Project',
+    'vacancies_project' => 'Vacancies for Project',
     'contacts' => 'Contacts',
     'send_resume' => 'Send Resume'
 ];
