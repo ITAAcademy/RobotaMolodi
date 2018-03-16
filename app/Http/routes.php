60d6feb09bcb190dd1cb000229c05255801ae42a
zakat <zakat@MBP-Iurii.Dlink>
2018-03-16 13:31:27 +0200

    rolled back changes regarding vendor

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -93,6 +93,7 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth','admin']], function (
     Route::get('/sliders/updatePublished/{slider_id}', 'Admin\SliderController@updatePublished');
     Route::resource('/seo-module', 'Admin\SeoModuleController');
     Route::resource('/projects', 'Admin\ProjectsController');
+    Route::post('upartimg', ['as'=>'upartimg', 'uses' => 'UploadFile@addArticleContent']);
 });
 
 Route::post('/slider/category', ['as' => 'slidersByCategory','uses' => 'SliderController@byCategory']);
