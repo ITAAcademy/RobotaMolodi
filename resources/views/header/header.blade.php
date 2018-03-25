7324a1a8ef76d5707444c553ddeed39e9bbccf62
zahar <zaharlistru@gmail.com>
2018-03-25 16:35:51 +0300

    Must be opportunity choose vacancy which was created before when user is adding vacancy in the page of create project

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -1,20 +1,20 @@
 <header>
     <nav class="navbar navbar-default col-xs-12">
         <div class="row">
-        <div class="col-xs-1 col-md-4 bars-left-modal" style="margin:0; padding:0">
-            <button type="button" class="btn btn-default only-bars">
-                <i class="fa fa-bars fa-lg" aria-hidden="true"></i>
-            </button>
-        </div>
-        <div class="col-xs-5 col-md-4 center-block">
-            <div class="header-logo text-center">
-                <a href="{{ url('/') }}" class="afterChange">{!! Html::image('image/logo-img.png',trans('header.home'),
-                    ['class'=>'img-responsive main-img col-xs-3'])!!}
-                    {!! Html::image('image/logo2.png',trans('header.home'),
-                        ['class'=>'img-responsive main-img col-xs-9'])!!}</a>
+            <div class="col-xs-3 col-md-4 bars-left-modal" style="margin:0; padding:0">
+                <button type="button" class="btn btn-default only-bars">
+                    <i class="fa fa-bars fa-lg" aria-hidden="true"></i>
+                </button>
             </div>
-        </div>
-        <div class="col-xs-offset-6">
+            <div class="col-xs-5 col-md-4 center-block">
+                <div class="header-logo text-center">
+                    <a href="{{ url('/') }}" class="afterChange">{!! Html::image('image/logo-img.png',trans('header.home'),
+                    ['class'=>'img-responsive main-img col-xs-3'])!!}
+                        {!! Html::image('image/logo2.png',trans('header.home'),
+                            ['class'=>'img-responsive main-img col-xs-9'])!!}</a>
+                </div>
+            </div>
+            <div class="col-xs-offset-6">
                 <span>| </span>
                 <a class="local-ua" href="{{url()}}/language/ua">ua</a>
                 <span> | </span>
@@ -22,24 +22,26 @@
                 <span> | </span>
             </div>
 
-        @if (Auth::guest())
-        <div class="row">
-        <div id="navregenterbutn" class="col-xs-6 col-md-4 navtab-registraion">
-            <button type="button" class="btn btn-default modal-enter col-xs-6">
-                <span>{!! Html::image('image/entry.png',trans('auth.signin'),['id'=>'entry']) !!}</span>
-                <span>{{ trans('auth.signin') }}</span>
-            </button>
-            <button type="button" class="btn btn-default modal-regestry col-xs-6">
-                <span>{!! Html::image('image/registry.png',trans('auth.signup'),['id'=>'registry']) !!}</span>
-                <span>{{ trans('auth.signup') }}</span>
-            </button>
-        </div>
-</div>
-        @else
-            <div class="col-xs-4 navtab-exit">
-                <div class="row pull-left">
-                    <a class="modal-user-button" href="/user/{{ Auth::user()->id }}/edit"><i class="fa fa-pencil" aria-hidden="true"></i></a>
-                    <a class="modal-user-button" @if(Auth::user()->isAdmin()) href="{{url('/admin')}}" @else href="{{ url('/cabinet') }}" @endif>
+            @if (Auth::guest())
+                <div class="row">
+                    <div id="navregenterbutn" class="col-xs-6 col-md-4 navtab-registraion">
+                        <button type="button" class="btn btn-default modal-enter col-xs-6">
+                            <span>{!! Html::image('image/entry.png',trans('auth.signin'),['id'=>'entry']) !!}</span>
+                            <span>{{ trans('auth.signin') }}</span>
+                        </button>
+                        <button type="button" class="btn btn-default modal-regestry col-xs-6">
+                            <span>{!! Html::image('image/registry.png',trans('auth.signup'),['id'=>'registry']) !!}</span>
+                            <span>{{ trans('auth.signup') }}</span>
+                        </button>
+                    </div>
+                </div>
+            @else
+                <div class="col-xs-4 navtab-exit">
+                    <div class="row pull-left">
+                        <a class="modal-user-button" href="/user/{{ Auth::user()->id }}/edit"><i class="fa fa-pencil"
+                                                                                                 aria-hidden="true"></i></a>
+                        <a class="modal-user-button" @if(Auth::user()->isAdmin()) href="{{url('/admin')}}"
+                           @else href="{{ url('/cabinet') }}" @endif>
                         <span class="img-user">
                             @if(Auth::user()->avatar and File::exists(public_path(Auth::user()->getAvatarPath())))
                                 {!! Html::image( asset(Auth::user()->getAvatarPath()), 'logo',
@@ -48,22 +50,22 @@
                                 {!! Html::image('image/m.jpg', 'logo', array( 'width' => '100%', 'height' => '100%')) !!}
                             @endif
                         </span>
-                        <span class="img-user-name">
+                            <span class="img-user-name">
                             <p>{{ Auth::user()->name }}</p>
-                            @if(Auth::user()->isAdmin())<p style="color: red">(Admin)</p>@endif
+                                @if(Auth::user()->isAdmin())<p style="color: red">(Admin)</p>@endif
                         </span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xs-2 navtab-exit" style="padding-top:10px">
+                    <a href="{{ url('/auth/logout') }}">
+                        <button type="button" class="btn btn-default modal-exit-button">
+                            <i class="fa fa-sign-out fa-lg fa-rotate-180" aria-hidden="true"></i>
+                            <span>{{trans('auth.signout')}}</span>
+                        </button>
                     </a>
                 </div>
-            </div>
-            <div class="col-xs-2 navtab-exit" style="padding-top:10px">
-                <a href="{{ url('/auth/logout') }}">
-                    <button type="button" class="btn btn-default modal-exit-button">
-                        <i class="fa fa-sign-out fa-lg fa-rotate-180" aria-hidden="true"></i>
-                        <span>{{trans('auth.signout')}}</span>
-                    </button>
-                </a>
-            </div>
-        @endif
+            @endif
         </div>
     </nav>
 </header>
@@ -98,7 +100,7 @@
             tab_content[0].style.borderRadius = "15px 0 15px 15px";
         });
 
-        $('.modal-enter,.btn-modal-enter' ).click(function () {
+        $('.modal-enter,.btn-modal-enter').click(function () {
             $('#rightModal').modal({
                 show: true,
                 keyboard: true
