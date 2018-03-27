552d64e812defb1a26699497d537b424b4ef2dfa
Julia <yulenkavip@gmail.com>
2018-03-27 13:38:11 +0300

    pagination for users

--- resources/views/newDesign/admin/users/index.blade.php
+++ resources/views/newDesign/admin/users/index.blade.php
@@ -41,5 +41,17 @@
       </tr>
       @endforeach
     </table>
+    <div class="center">
+  <div class="pagination">
+    <a href="#">&laquo;</a>
+    <a href="#">1</a>
+    <a href="#" class="active">2</a>
+    <a href="#">3</a>
+    <a href="#">4</a>
+    <a href="#">5</a>
+    <a href="#">6</a>
+    <a href="#">&raquo;</a>
+  </div>
+</div>
 </div>
 @endsection
