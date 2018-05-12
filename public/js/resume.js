8d5d96cbd7e74e29adf276acab53d4242990b8d9
Andrey Zakrevsky <zak111revsky@gmail.com>
2018-05-12 16:47:03 +0300

    optimization

--- public/js/resume.js
+++ public/js/resume.js
@@ -33,9 +33,8 @@
 
     });
 
-
     function validElem(el) {
-        if(el ==48 ||el ==49 || el ==50 || el ==51|| el ==52|| el ==53|| el ==54|| el ==55|| el ==56|| el ==57){
+        if(el>=48 && el<=57){
             return true;
         }
         else return false;
