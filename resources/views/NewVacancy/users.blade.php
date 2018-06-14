09569a5ca655f995a4781275ba1693f33a0c260e
Daria Halandzovskaya <daria.halandzovskaya@gmail.com>
2018-06-14 12:18:47 +0300

    fixed mobile view (company/create) page

--- resources/views/NewVacancy/users.blade.php
+++ resources/views/NewVacancy/users.blade.php
@@ -1,23 +1,19 @@
 @extends ('NewVacancy/layout')
 
 @section ('contents')
-
     <div>
         <ul class="nav nav-tabs">
-            <li role = "presentation">{!!link_to_route('vacancy.index','Мої вакансії')!!}</li>
+            <li role="presentation">{!!link_to_route('vacancy.index','Мої вакансії')!!}</li>
 
         </ul>
     </div>
-
     <div>
         <ul class="nav nav-tabs">
-            <li role = "presentation">{!!link_to_route('vacancy.create','Створити вакансію')!!}</li>
-            <li role = "presentation">{!!link_to_route('vacancy.edit','Редагувати вакансію')!!}</li>
-            <li role = "presentation">{!!link_to_route('vacancy.destroy','Видалити вакансію')!!}</li>
-            <li role = "presentation">{!!link_to_route('company.create','Створити компанію')!!}</li>
-            <li role = "presentation">{!!link_to_route('company.edit','Редагувати компанію')!!}</li>
-
+            <li role="presentation">{!!link_to_route('vacancy.create','Створити вакансію')!!}</li>
+            <li role="presentation">{!!link_to_route('vacancy.edit','Редагувати вакансію')!!}</li>
+            <li role="presentation">{!!link_to_route('vacancy.destroy','Видалити вакансію')!!}</li>
+            <li role="presentation">{!!link_to_route('company.create','Створити компанію')!!}</li>
+            <li role="presentation">{!!link_to_route('company.edit','Редагувати компанію')!!}</li>
         </ul>
     </div>
-
 @stop
