6d59bfe651515c3b15a6209847fcc3ac43a9d156
Anna Overchuk <annaoverchuk55@gmail.com>
2018-06-21 16:48:22 +0300

    footer

--- resources/views/footer/footer.blade.php
+++ resources/views/footer/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="inner-footer col-sm-6 col-md-3">
             <ul class="footer-list">
                 <li class="li_header"><img src="{{asset('image/redHat.png')}}">{{ trans('footer.information') }}</li>
-                <hr>
+                <hr style="width: 243px;">
                 <li><a href="aboutus">{{ trans('footer.about') }}</a></li>
                 <li><a href="https://profitday.info/allcompanies" target="_blank">{{ trans('footer.partners') }}</a></li>
             </ul>
@@ -12,7 +12,7 @@
         <div class="inner-footer col-sm-6 col-md-3">
             <ul class="footer-list">
                 <li class="li_header"><img src="{{asset('image/redHat.png')}}">{{ trans('footer.education') }}</li>
-                <hr>
+                <hr style="width: 243px;">
                 <li><a href="https://profitday.info/upcomingevents" target="_blank">{{ trans('footer.training') }}</a></li>
                 <li><a href="https://intita.com/courses" target="_blank">{{ trans('footer.courses') }}</a></li>
             </ul>
@@ -20,7 +20,7 @@
         <div class="inner-footer col-sm-6 col-md-3">
             <ul class="footer-list">
                 <li class="li_header"><img src="{{asset('image/redHat.png')}}">{{ trans('footer.service') }}</li>
-                <hr>
+                <hr style="width: 243px;">
                 <li><a href="https://intita.com" target="_blank">{{ trans('footer.education') }}</a></li>
             </ul>
         </div>
@@ -39,11 +39,11 @@
     <div class="down-footer col-xs-12">
         <div class="inner-footer col-md-7">
             <span>{{ trans('footer.copy') }}</span>
-            <img src="{{asset('image/vidrodjenja.png')}}">
+            <img src="{{asset('image/vidrodjenja.png')}}" style="padding: 17px">
         </div>
         <div class="inner-footer col-md-5">
-            <div class="row">
-                <div class="col-xs-5 col-sm-4">
+            <div class="row" style="padding: 2px">
+                <div class="col-xs-5 col-sm-4" style="padding: 0">
                     <span>{{ trans('footer.share') }}</span>
                 </div>
                 <div class="col-xs-1 col-sm-2">
