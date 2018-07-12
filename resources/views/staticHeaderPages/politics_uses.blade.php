eb73e2b0e2216d30b6676567e5726fb886c476d8
MinionsBoy <venom.nfs.ru@gmail.com>
2018-07-12 12:56:06 +0300

    Change css

--- resources/views/staticHeaderPages/politics_uses.blade.php
+++ resources/views/staticHeaderPages/politics_uses.blade.php
@@ -6,26 +6,26 @@
                ['url'=> 'head','name'=>'Головна'],
                ['name' => 'Политика использования', 'url' => false]
                ]))
-        <article>
+        <article class="mainArticle">
 
-            <strong><h4>{{trans('policy.structure')}}:</h4></strong>
-            <em><span>{{trans('policy.personalD')}} – </span>{{trans('policy.personalData')}}.</em><br>
-            <ol>
+            <strong><h4 class="infoPolicy">{{trans('policy.structure')}}:</h4></strong>
+            <em class="policyLocation"><span class="personalData">{{trans('policy.personalD')}} – </span>{{trans('policy.personalData')}}.</em><br>
+            <ol class="addressPolicy">
                 <li>{{trans('policy.fullName')}}.</li>
                 <li>{{trans('policy.address')}}.</li>
-                <li>{{trans('policy.crn')}} {{ trans('form.regcomapany') }}у.</li>
+                <li>{{trans('policy.crn')}} {{ trans('form.regcomapany') }}.</li>
                 <li>{{trans('policy.mail')}}.</li>
                 <li>{{trans('policy.other')}}.</li>
             </ol>
-            <h4>{{trans('policy.owner')}}:</h4>
-            <p>{{trans('policy.location')}}.<br>
+            <h4 class="infoPolicy">{{trans('policy.owner')}}:</h4>
+            <p class="policyLocation">{{trans('policy.location')}}.<br>
                 {{ trans('main.phone') }} +38 (097) 934-25-24<br>
                 E-mail: <a href="mailto:robotamolodi@gmail.com">robotamolodi@gmail.com</a><br>
                 Website: <a href="https://robotamolodi.org">www.robotamolodi.org</a></p><br>
-            <h4>{{trans('policy.dataprocessing')}}:</h4>
-            <p>{{trans('policy.consent')}}</p>
-            <h4>{{trans('policy.rightTo')}}</h4>
-            <ul>
+            <h4 class="infoPolicy">{{trans('policy.dataprocessing')}}:</h4>
+            <p class="policyLocation">{{trans('policy.consent')}}</p>
+            <h4 class="infoPolicy">{{trans('policy.rightTo')}}</h4>
+            <ul class="rightPolicy">
                 <li>{{trans('policy.right1')}};</li>
                 <li>{{trans('policy.right2')}};</li>
                 <li>{{trans('policy.right3')}};</li>
@@ -40,16 +40,16 @@
                 <li>{{trans('policy.right12')}};</li>
                 <li>{{trans('policy.right13')}}.</li>
             </ul><br>
-            <h4>{{trans('policy.collecting')}}:</h4>
-            <ul>
+            <h4 class="infoPolicy">{{trans('policy.collecting')}}:</h4>
+            <ul class="rightPolicy">
                 <li>{{trans('policy.purpose1')}};</li>
                 <li>{{trans('policy.purpose2')}};</li>
                 <li>{{trans('policy.purpose3')}}.</li>
             </ul><br>
-            <h4>{{trans('policy.receiveInformation')}}:</h4>
-            <p>{{trans('policy.consentToReceive')}}.</p><br>
-            <h4>{{trans('policy.personsWhoTransfer')}}:</h4>
-            <p>{{trans('policy.transfer')}}.
+            <h4 class="infoPolicy">{{trans('policy.receiveInformation')}}:</h4>
+            <p class="policyLocation">{{trans('policy.consentToReceive')}}.</p><br>
+            <h4 class="infoPolicy">{{trans('policy.personsWhoTransfer')}}:</h4>
+            <p class="policyLocation">{{trans('policy.transfer')}}.
                 <em>{{trans('policy.guarantees')}}.</em></p>
         </article>
     </div>
