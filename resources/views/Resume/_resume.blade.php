373d549437c05322b440d25e9a12c7c848f4bfb2
Sergey Lysov <serglysov@gmail.com>
2018-07-15 23:56:59 +0300

    fixed css for cabinet view

--- resources/views/Resume/_resume.blade.php
+++ resources/views/Resume/_resume.blade.php
@@ -23,7 +23,7 @@
                     @include('newDesign.socialModule.share-btn-block' , ['url' => URL::asset('/resume/'.$resume->id) ])
                 @endif
             </div>
-            <div class="col-xs-12 col-md-10">
+            <div class="col-xs-12 col-md-10 topResText">
                 <div class="panel-heading-cabinet">
                     <p class="position-cabinet">
                         <a class="orangColor-cabinet-name" href="{{route('resume.show', $resume->id)}}">{!!$resume->position!!}</a>
@@ -46,7 +46,7 @@
         </div>
         <div class="row">
             <div class="col-md-2"></div>
-            <div class="col-md-10">
+            <div class=" col-md-10">
                 <div class="col-xs-12 col-md-3">
                     <a class="orangColor-cabinet" href="/resume/{{$resume->id}}/destroy" onclick="return ConfirmDelete();">
                         <i class="fa fa-trash" aria-hidden="true"></i>
