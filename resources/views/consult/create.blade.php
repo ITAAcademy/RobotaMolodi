4bdc2986a845d1dd587c0183a2af5d96bbe338c0
Daria Halandzovskaya <daria.halandzovskaya@gmail.com>
2018-07-20 17:25:21 +0300

    add calendar

--- resources/views/consult/create.blade.php
+++ resources/views/consult/create.blade.php
@@ -23,13 +23,13 @@
                 <div class="col-md-5 col-sm-5">
                     <select class="inputPlace2" id="city_id" name="city">
                         @foreach($cities as $city)
-                                <option value="{{$city->name}}">{{$city->name}}</option>
+                            <option value="{{$city->name}}">{{$city->name}}</option>
                         @endforeach
                     </select>
                 </div>
                 {{--<div class="col-md-5">--}}
-                    {{--<input type="text" placeholder="{{ trans('main.city') }}" class="form-control" name="city"--}}
-                           {{--value="{{ old('city') }}">--}}
+                {{--<input type="text" placeholder="{{ trans('main.city') }}" class="form-control" name="city"--}}
+                {{--value="{{ old('city') }}">--}}
                 {{--</div>--}}
             </div>
 
@@ -40,13 +40,13 @@
                 <div class="col-md-5 col-sm-5">
                     <select class="inputPlace2" id="industry_id" name="area">
                         @foreach($industries as $industry)
-                                <option value="{{$industry->name}}">{{$industry->name}}</option>
+                            <option value="{{$industry->name}}">{{$industry->name}}</option>
                         @endforeach
                     </select>
                 </div>
                 {{--<div class="col-md-5">--}}
-                    {{--<input type="text" placeholder="{{ trans('main.area') }}" class="form-control" name="area"--}}
-                           {{--value="{{ old('area') }}">--}}
+                {{--<input type="text" placeholder="{{ trans('main.area') }}" class="form-control" name="area"--}}
+                {{--value="{{ old('area') }}">--}}
                 {{--</div>--}}
             </div>
 
@@ -61,26 +61,40 @@
             </div>
 
             {{--<div class="form-group">--}}
-                {{--<div class="col-md-5">--}}
-                    {{--<input type="text" placeholder="Вартість консультації" class="form-control" name="cost"--}}
-                           {{--value="{{ old('cost') }}">--}}
-                {{--</div>--}}
+            {{--<div class="col-md-5">--}}
+            {{--<input type="text" placeholder="Вартість консультації" class="form-control" name="cost"--}}
+            {{--value="{{ old('cost') }}">--}}
+            {{--</div>--}}
             {{--</div>--}}
 
-            <div class="form-group row row">
+            <div class="form-group row">
                 <label for="description" class="col-md-3 col-sm-3 label-text-company">
                     {{ trans('main.description') }}
                 </label>
                 <div class="col-md-5">
-                    <textarea rows="5" cols="40" placeholder="{{ trans('main.description') }}" class="form-control" name="description"
-                           value="{{ old('description') }}"></textarea>
+                    <textarea rows="5" cols="40" placeholder="{{ trans('main.description') }}" class="form-control"
+                              name="description"
+                              value="{{ old('description') }}"></textarea>
+                </div>
+            </div>
+
+            <div class="row">
+                <label for="description" class="col-md-3 col-sm-3 label-text-company">
+                    Виберiть дату:
+                </label>
+                <div class="col-md-5 block-calendar">
+                    <div style="overflow:hidden;">
+                        <div class="form-group">
+                            <div id="datetimepicker"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="form-group row">
                 <div class="col-md-5">
-                    <label for="img">Ваш аватар Ще не працює</label>
-                    <input id="image" type="file"  name="img">
+                    <label for="img">Ваш аватар ще не працює</label>
+                    <input id="image" type="file" name="img">
                 </div>
             </div>
 
