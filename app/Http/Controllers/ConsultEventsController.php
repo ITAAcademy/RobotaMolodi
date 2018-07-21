472b95e1299b23099066417e8f853d91fe8b4f70
Sergey Lysov <serglysov@gmail.com>
2018-07-22 02:08:50 +0300

    created calendar for consultant events, created controllers for events ajax

---
+++ app/Http/Controllers/ConsultEventsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Consult;
+
+class ConsultEventsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        $consultant = Consult::all();
+        //if($request->isAjax()){
+            return json_encode($consultant);
+       //}
+    }
+
+}
---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Consult;
+
+class EventsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+
+
+}
